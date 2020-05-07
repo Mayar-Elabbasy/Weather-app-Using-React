9cdd085f20e27bdb2488f4c7c3b8721854897132
Mayar Elabbasy <mayarelabbasy2016@gmail.com>
2020-05-07 16:45:20 +0200

    added weather component & installed bootstrap

---
+++ src/components/weather.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Weather extends Component {
+    // state = {  }
+    render() { 
+        return ( 
+        <div className="container">
+            <div className="cards">
+                <h1>Egypt</h1>
+            </div>
+        </div>
+        );
+    }
+}
+ 
+export default Weather;
