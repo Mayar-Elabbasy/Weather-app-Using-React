41ffd554b2640d1b29862a0cf0afa1ee0e1f0185
Mayar Elabbasy <mayarelabbasy2016@gmail.com>
2020-05-08 17:49:21 +0200

    added final design

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -3,27 +3,27 @@ import React from 'react';
 const Form = props =>{
     return(
      <div className="container">
-        <form className="mt-5" onSubmit= {props.loadWeather}>
+        <form className="m-5 bg-warning p-1" onSubmit= {props.loadWeather}>
         <div>{props.error ? error() : null}</div>
             <div className="row">
-                <div className="col-md-4">            
+                <div className="col-md-4 m-2">            
                     <input type="text"
-                     className="form-control m-3"
+                     className="form-control text-info"
                       name="city"
                       placeholder="City" 
                       autoComplete="on"/>
                 </div> 
-                <div className="col-md-4">
+                <div className="col-md-4 m-2">
                     <input
                     type="text"
-                    className="form-control m-3"
+                    className="form-control text-info"
                     placeholder="Country"
                     name="country"
                     autoComplete="on"
                     />
                 </div> 
-                <div className="col-md-3 m-3">            
-                    <button className="form-control btn btn-warning">Get Weather</button>
+                <div className="col-md-3 m-2">            
+                    <button className="form-control btn btn-info">Get Weather</button>
                 </div>  
             </div>
         </form>
